28144d1aeeb1e81e2ad5a8f585907a8266c842b8
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-07-10 14:11:28 +0100

    Output data to console

--- main.go
+++ main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
 )
+
+type Weather struct {
+	Location struct {
+		Name      string `json:"name"`
+		Country   string `json:"country"`
+		Localtime string `json:"localtime"`
+	} `json:"location"`
+
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch    int64   `json:"time_epoch"`
+				TempC        float64 `json:"temp_c"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+				Condition    struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
 
 func main() {
 	requestUrl := "http://api.weatherapi.com/v1/forecast.json"
 	apiKey := "c26061f634ea4804890113753241007"
-	city := "lagos"
 	days := "3"
+	var city string
+
+	if len(os.Args) > 1 {
+		city = os.Args[1]
+	} else {
+		city = "lagos"
+	}
 
 	res, err := http.Get(fmt.Sprintf("%s?key=%s&q=%s&days=%s&alerts=no&aqi=no", requestUrl, apiKey, city, days))
 
@@ -20,8 +59,6 @@ func main() {
 
 	defer res.Body.Close()
 
-	fmt.Println(res.StatusCode)
-
 	if res.StatusCode != 200 {
 		panic("Weather api not available")
 	}
@@ -30,5 +67,32 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(body))
+
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf("%s, %s: %.0fC, %s\n", location.Name, location.Country, current.TempC, current.Condition.Text)
+
+	for _, hour := range hours {
+		date := time.Unix(hour.TimeEpoch, 0)
+
+		if date.Before(time.Now()) {
+			continue
+		}
+
+		message := fmt.Sprintf("%s - %.0fC, %.0f%%, %s\n", date.Format("15:04"), hour.TempC, hour.ChanceOfRain, hour.Condition.Text)
+
+		if hour.ChanceOfRain < 20 {
+			color.Yellow(message)
+		} else if hour.ChanceOfRain > 70 {
+			color.Cyan(message)
+		} else {
+			fmt.Print(message)
+		}
+	}
 }
